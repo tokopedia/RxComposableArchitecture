a420ac574888de3e279fdab2ca10a5be567a38da
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2022-04-08 11:01:45 +0800

    add observeOn

--- RxComposableArchitecture/Classes/Effects/Deferring.swift
+++ RxComposableArchitecture/Classes/Effects/Deferring.swift
@@ -10,19 +10,18 @@ import RxSwift
 extension Effect {
     /// Returns an effect that will be executed after given `dueTime`.
     ///
-    ///     case let .textChanged(text):
-    ///       struct SearchId: Hashable {}
-    ///
-    ///     case let .textChanged(text):
-    ///       struct SearchId: Hashable {}
-    ///
-    ///       return environment.search(text)
-    ///         .map(Action.searchResponse)
-    ///         .deferred(for: .milliseconds(500), scheduler: environment.mainQueue)
+    /// ```swift
+    /// case let .textChanged(text):
+    ///   return environment.search(text)
+    ///     .deferred(for: 0.5, scheduler: environment.mainQueue)
+    ///     .map(Action.searchResponse)
+    /// ```
     ///
     /// - Parameters:
-    ///   - for: The duration you want to deferred for.
-    ///   - scheduler: The scheduler you want to deliver the deferred output to.
+    ///   - upstream: the effect you want to defer.
+    ///   - dueTime: The duration you want to defer for.
+    ///   - scheduler: The scheduler you want to deliver the defer output to.
+    ///   - options: Scheduler options that customize the effect's delivery of elements.
     /// - Returns: An effect that will be executed after `dueTime`
     public func deferred(
         for dueTime: RxTimeInterval,
@@ -30,7 +29,7 @@ extension Effect {
     ) -> Effect<Element> {
         Observable.just(())
             .delay(dueTime, scheduler: scheduler)
-            .flatMap { self }
+            .flatMap { self.observeOn(scheduler) }
             .eraseToEffect()
     }
 }
