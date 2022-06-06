176ac08533b4bdcfe7e3287624509c7730d14229
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2022-06-06 11:06:36 +0800

    Add ifLet and Reducer Optional example (#19)
    * add IfLet Example
    
    * add spacing
    
    * update code owners

---
+++ Examples/Examples/5-OptionalIfLet/OptionalIfLetVC+Reducer.swift
@@ -0,0 +1,42 @@
+//
+//  OptionalIfLetVC+Reducer.swift
+//  Examples
+//
+//  Created by jefferson.setiawan on 06/06/22.
+//
+
+import CasePaths
+import RxComposableArchitecture
+
+internal struct OptionalIfLetState: Equatable {
+    internal var number = 0
+    internal var counter: CounterState?
+}
+
+internal enum OptionalIfLetAction: Equatable {
+    case didToggle
+    case counter(CounterAction)
+}
+
+internal let optionalIfLetReducer = Reducer<OptionalIfLetState, OptionalIfLetAction, Void>.combine(
+    pullbackCounterReducer.optional()
+        .pullback(
+            state: \.counter,
+            action: /OptionalIfLetAction.counter,
+            environment: { _ in }
+    ),
+    Reducer { state, action, _ in
+        switch action {
+        case .didToggle:
+            if let counter = state.counter {
+                state.number = counter.number
+                state.counter = nil
+            } else {
+                state.counter = CounterState(number: state.number)
+            }
+            return .none
+        case .counter: return .none
+        }
+    }
+
+)
---
+++ Examples/ExamplesTests/OptionalIfLetReducerTests.swift
@@ -0,0 +1,32 @@
+//
+//  OptionalIfLetReducerTests.swift
+//  ExamplesTests
+//
+//  Created by jefferson.setiawan on 06/06/22.
+//
+
+import XCTest
+import RxComposableArchitecture
+import RxSwift
+@testable import Examples
+
+class OptionalIfLetReducerTests: XCTestCase {
+    func testDidSwitchToggle() {
+        let testStore = TestStore(initialState: OptionalIfLetState(), reducer: optionalIfLetReducer, environment: ())
+        testStore.send(.didToggle) {
+            $0.counter = CounterState()
+        }
+    }
+    
+    func testChangeCounterThenToggle() {
+        let testStore = TestStore(initialState: OptionalIfLetState(number: 0, counter: CounterState(number: 10)), reducer: optionalIfLetReducer, environment: ())
+        testStore.send(.counter(.didTapPlus)) {
+            $0.counter!.number = 11
+        }
+        
+        testStore.send(.didToggle) {
+            $0.number = 11
+            $0.counter = nil
+        }
+    }
+}
