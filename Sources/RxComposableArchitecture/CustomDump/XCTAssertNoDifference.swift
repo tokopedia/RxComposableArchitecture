576248ab1080382358a0b466b1184c0c58ff9ee7
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2022-08-03 09:54:50 +0700

    add if debug on the public XCTAssertNoDifference

--- Sources/RxComposableArchitecture/CustomDump/XCTAssertNoDifference.swift
+++ Sources/RxComposableArchitecture/CustomDump/XCTAssertNoDifference.swift
@@ -37,6 +37,7 @@
 ///     you call this function.
 ///   - line: The line number where the failure occurs. The default is the line number where you
 ///     call this function.
+#if DEBUG
 public func XCTAssertNoDifference<T>(
   _ expression1: @autoclosure () throws -> T,
   _ expression2: @autoclosure () throws -> T,
@@ -87,3 +88,4 @@ public func XCTAssertNoDifference<T>(
     )
   }
 }
+#endif
