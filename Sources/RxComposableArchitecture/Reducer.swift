9cc87d6349e76e73557a6534652847f506577b0b
Jefferson Setiawan <jefferson.setiawan@tokopedia.com>
2022-08-03 12:20:36 +0700

    Regress: Fix Bootstrap not called (#40)
    * remove callAsFunction, focus on func run
    
    * remove inline function
    
    * add Bootstrap test

--- Sources/RxComposableArchitecture/Reducer.swift
+++ Sources/RxComposableArchitecture/Reducer.swift
@@ -536,26 +536,6 @@ public struct Reducer<State, Action, Environment> {
         }
     }
 
-    public func callAsFunction(
-        _ state: inout State,
-        _ action: Action,
-        _ environment: Environment
-    ) -> Effect<Action> {
-        func environmentToUse() -> Environment {
-            #if DEBUG
-                if let bootstrappedEnvironment = Bootstrap.get(environment: type(of: environment)) {
-                    return bootstrappedEnvironment
-                } else {
-                    return environment
-                }
-            #else
-                return environment
-            #endif
-        }
-
-        return reducer(&state, action, environmentToUse())
-    }
-
     public func combined(with other: Reducer) -> Reducer {
         .combine(self, other)
     }
@@ -571,22 +551,13 @@ public struct Reducer<State, Action, Environment> {
     public func run(
         _ state: inout State,
         _ action: Action,
-        _ environment: Environment,
-        _ debug: (State) -> Void = { _ in }
+        _ environment: Environment
     ) -> Effect<Action> {
-        let reducer = self.reducer(&state, action, environment)
-        debug(state)
-
-        return reducer
-    }
-}
-
-extension Reducer where Environment == Void {
-    public func callAsFunction(
-        _ state: inout State,
-        _ action: Action
-    ) -> Effect<Action> {
-        callAsFunction(&state, action, ())
+        #if DEBUG
+            reducer(&state, action, Bootstrap.get(environment: type(of: environment)) ?? environment)
+        #else
+            reducer(&state, action, environment)
+        #endif
     }
 }
 
