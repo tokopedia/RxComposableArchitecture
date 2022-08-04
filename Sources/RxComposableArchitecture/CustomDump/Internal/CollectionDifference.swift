04badec780b36754a907c0dd0fb9e813e2c24f8b
Benny Kurniawan <41714182+kwekkwek@users.noreply.github.com>
2022-08-04 10:37:19 +0700

    [RxTCA] implement custom dump (#39)
    * adding custom dump
    
    * finish!
    
    * re-add diffmode
    
    * add if debug on the public XCTAssertNoDifference
    
    Co-authored-by: Jefferson Setiawan <jefferson.setiawan@tokopedia.com>

---
+++ Sources/RxComposableArchitecture/CustomDump/Internal/CollectionDifference.swift
@@ -0,0 +1,8 @@
+extension CompatibilityCollectionDifference.Change {
+  var offset: Int {
+    switch self {
+    case let .insert(offset, _, _), let .remove(offset, _, _):
+      return offset
+    }
+  }
+}
