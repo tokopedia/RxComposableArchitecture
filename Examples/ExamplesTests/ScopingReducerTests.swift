c6d65314dbaffd1c232883cb2c30437a43c05d9d
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2022-09-12 17:43:18 +0700

    add new store scoping

--- Examples/ExamplesTests/ScopingReducerTests.swift
+++ Examples/ExamplesTests/ScopingReducerTests.swift
@@ -12,7 +12,7 @@ import RxSwift
 
 class ScopingReducerTests: XCTestCase {
     func testTapPlus() {
-        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: ())
+        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: (), useNewScope: true)
         
         testStore.send(.counter(.didTapPlus)) {
             $0.counter.number = 1
@@ -20,7 +20,7 @@ class ScopingReducerTests: XCTestCase {
     }
     
     func testTapMinus() {
-        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: ())
+        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: (), useNewScope: true)
         
         testStore.send(.counter(.didTapMinus)) {
             $0.counter.number = -1
@@ -28,7 +28,7 @@ class ScopingReducerTests: XCTestCase {
     }
     
     func testTapJumpButton() {
-        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: ())
+        let testStore = TestStore(initialState: ScopingState(), reducer: scopingReducer, environment: (), useNewScope: true)
         
         testStore.send(.didTapJump) {
             $0.counter.number = 100
