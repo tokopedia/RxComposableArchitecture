97ce0997ceeb318b9c47c1f3d7e327b4100e18e3
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2022-10-19 00:16:34 +0700

    update cancellation.swift

--- Sources/RxComposableArchitecture/Effects/Cancellation.swift
+++ Sources/RxComposableArchitecture/Effects/Cancellation.swift
@@ -5,9 +5,10 @@ extension Effect {
     /// Turns an effect into one that is capable of being canceled.
     ///
     /// To turn an effect into a cancellable one you must provide an identifier, which is used in
-    /// `Effect.cancel(id:)` to identify which in-flight effect should be canceled. Any hashable
-    /// value can be used for the identifier, such as a string, but you can add a bit of protection
-    /// against typos by defining a new type that conforms to `Hashable`, such as an empty struct:
+    /// ``Effect/cancel(id:)-iun1`` to identify which in-flight effect should be canceled. Any
+    /// hashable value can be used for the identifier, such as a string, but you can add a bit of
+    /// protection against typos by defining a new type for the identifier, or by defining a custom
+    /// hashable type:
     ///
     ///     struct LoadUserId: Hashable {}
     ///
@@ -27,52 +28,73 @@ extension Effect {
     ///     canceled before starting this new one.
     /// - Returns: A new effect that is capable of being canceled by an identifier.
     public func cancellable(id: AnyHashable, cancelInFlight: Bool = false) -> Effect {
-        let effect = Observable<Action>.deferred {
+        Observable.deferred {
             cancellablesLock.lock()
             defer { cancellablesLock.unlock() }
-
-            let subject = PublishSubject<Action>()
-            /// Workaround for testing `testEffectSubscriberInitializer_WithCancellation`
-            var values: [Action] = []
-            var isCaching = true
-            let disposable =
-                self
-                    .do(onNext: { val in
-                        guard isCaching else { return }
-                        values.append(val)
-                    })
-                    .subscribe(subject)
-            var cancellationDisposable: AnyDisposable!
-            cancellationDisposable = AnyDisposable(
+            
+            let id = CancelToken(id: id)
+            if cancelInFlight {
+                cancellationCancellables[id]?.forEach { $0.dispose() }
+            }
+            
+            /// Flag is used to prevent disposable to send event on disposed `cancellationSubject`
+            /// Thanks: https://github.com/dannyhertz/rxswift-composable-architecture/issues/5
+            var hasCompleted = false
+            
+            let cancellationSubject = PublishSubject<Void>()
+            
+            var cancellationCancellable: AnyDisposable!
+            cancellationCancellable = AnyDisposable(
                 Disposables.create {
                     cancellablesLock.sync {
-                        subject.onCompleted()
-                        disposable.dispose()
-                        cancellationCancellables[id]?.remove(cancellationDisposable)
+                        if !hasCompleted {
+                            cancellationSubject.onNext(())
+                            cancellationSubject.onCompleted()
+                        }
+                        cancellationCancellables[id]?.remove(cancellationCancellable)
                         if cancellationCancellables[id]?.isEmpty == .some(true) {
                             cancellationCancellables[id] = nil
                         }
                     }
-            })
-
-            cancellationCancellables[id, default: []].insert(
-                cancellationDisposable
+                }
             )
-
-            return Observable.from(values)
-                .concat(subject)
+            
+            return self.takeUntil(cancellationSubject)
                 .do(
-                    onError: { _ in cancellationDisposable.dispose() },
-                    onCompleted: cancellationDisposable.dispose,
-                    onSubscribed: { isCaching = false },
-                    onDispose: cancellationDisposable.dispose
+                    onError: { _ in
+                        hasCompleted = true
+                        cancellationCancellable.dispose()
+                    },
+                    onSubscribed: {
+                        _ = cancellablesLock.sync {
+                            cancellationCancellables[id, default: []].insert(
+                                cancellationCancellable
+                            )
+                        }
+                    },
+                    onDispose: {
+                        hasCompleted = true
+                        cancellationCancellable.dispose()
+                    }
                 )
         }
         .eraseToEffect()
-
-        return cancelInFlight ? .concatenate(.cancel(id: id), effect) : effect
     }
-
+    
+    /// Turns an effect into one that is capable of being canceled.
+    ///
+    /// A convenience for calling ``Effect/cancellable(id:cancelInFlight:)-17skv`` with a static type
+    /// as the effect's unique identifier.
+    ///
+    /// - Parameters:
+    ///   - id: A unique type identifying the effect.
+    ///   - cancelInFlight: Determines if any in-flight effect with the same identifier should be
+    ///     canceled before starting this new one.
+    /// - Returns: A new effect that is capable of being canceled by an identifier.
+    public func cancellable(id: Any.Type, cancelInFlight: Bool = false) -> Effect {
+        self.cancellable(id: ObjectIdentifier(id), cancelInFlight: cancelInFlight)
+    }
+    
     /// An effect that will cancel any currently in-flight effect with the given identifier.
     ///
     /// - Parameter id: An effect identifier.
@@ -81,11 +103,54 @@ extension Effect {
     public static func cancel(id: AnyHashable) -> Effect {
         return .fireAndForget {
             cancellablesLock.sync {
-                cancellationCancellables[id]?.forEach { $0.dispose() }
+                cancellationCancellables[CancelToken(id: id)]?.forEach { $0.dispose() }
             }
         }
     }
+    
+    /// An effect that will cancel any currently in-flight effect with the given identifier.
+    ///
+    /// A convenience for calling ``Effect/cancel(id:)-iun1`` with a static type as the effect's
+    /// unique identifier.
+    ///
+    /// - Parameter id: A unique type identifying the effect.
+    /// - Returns: A new effect that will cancel any currently in-flight effect with the given
+    ///   identifier.
+    public static func cancel(id: Any.Type) -> Effect {
+        .cancel(id: ObjectIdentifier(id))
+    }
+    
+    /// An effect that will cancel multiple currently in-flight effects with the given identifiers.
+    ///
+    /// - Parameter ids: An array of effect identifiers.
+    /// - Returns: A new effect that will cancel any currently in-flight effects with the given
+    ///   identifiers.
+    public static func cancel(ids: [AnyHashable]) -> Effect {
+        .merge(ids.map(Effect.cancel(id:)))
+    }
+    
+    /// An effect that will cancel multiple currently in-flight effects with the given identifiers.
+    ///
+    /// A convenience for calling ``Effect/cancel(ids:)-dmwy`` with a static type as the effect's
+    /// unique identifier.
+    ///
+    /// - Parameter ids: An array of unique types identifying the effects.
+    /// - Returns: A new effect that will cancel any currently in-flight effects with the given
+    ///   identifiers.
+    public static func cancel(ids: [Any.Type]) -> Effect {
+        .merge(ids.map(Effect.cancel(id:)))
+    }
 }
 
-internal var cancellationCancellables: [AnyHashable: Set<AnyDisposable>] = [:]
+struct CancelToken: Hashable {
+    let id: AnyHashable
+    let discriminator: ObjectIdentifier
+    
+    init(id: AnyHashable) {
+        self.id = id
+        self.discriminator = ObjectIdentifier(type(of: id.base))
+    }
+}
+
+internal var cancellationCancellables: [CancelToken: Set<AnyDisposable>] = [:]
 internal let cancellablesLock = NSRecursiveLock()
