f3c9ea0e5540b5d01fc4b9a151f58d7769de1232
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2022-10-18 09:44:22 +0700

    update dependencies

--- Sources/RxComposableArchitecture/Dependencies/Dependencies/Context.swift
+++ Sources/RxComposableArchitecture/Dependencies/Dependencies/Context.swift
@@ -1,5 +1,17 @@
 extension DependencyValues {
   /// The current dependency context.
+  ///
+  /// The current ``DependencyContext`` can be used to determine how dependencies are loaded by the
+  /// current runtime.
+  ///
+  /// It can also be overridden, for example via ``withValue(_:_:operation:)-705n``, to control how
+  /// dependencies will be loaded by the runtime for the duration of the override.
+  ///
+  /// ```swift
+  /// DependencyValues.withValue(\.context, .preview) {
+  ///   // Dependencies accessed here default to their "preview" value
+  /// }
+  /// ```
   public var context: DependencyContext {
     get { self[DependencyContextKey.self] }
     set { self[DependencyContextKey.self] = newValue }
@@ -7,7 +19,7 @@ extension DependencyValues {
 }
 
 enum DependencyContextKey: DependencyKey {
-  static var liveValue = DependencyContext.live
-  static var previewValue = DependencyContext.preview
-  static var testValue = DependencyContext.test
+  static let liveValue = DependencyContext.live
+  static let previewValue = DependencyContext.preview
+  static let testValue = DependencyContext.test
 }
