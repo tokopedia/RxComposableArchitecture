40d90855a46a1048a5828dcc208159c1b3ee7ffa
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2022-10-20 20:50:17 +0700

    task result and task cancellable value

--- Sources/RxComposableArchitecture/Internal/TaskCancellableValue.swift
+++ Sources/RxComposableArchitecture/Internal/TaskCancellableValue.swift
@@ -1,8 +1,23 @@
-//
-//  File.swift
-//  
-//
-//  Created by daniel.istyana on 20/10/22.
-//
+extension Task where Failure == Error {
+    public var cancellableValue: Success {
+        get async throws {
+            try await withTaskCancellationHandler {
+                self.cancel()
+            } operation: {
+                try await self.value
+            }
+        }
+    }
+}
 
-import Foundation
+extension Task where Failure == Never {
+    public var cancellableValue: Success {
+        get async {
+            await withTaskCancellationHandler {
+                self.cancel()
+            } operation: {
+                await self.value
+            }
+        }
+    }
+}
