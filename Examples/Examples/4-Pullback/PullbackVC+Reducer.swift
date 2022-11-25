c3974afffdc94313ef94706f819f3be222a952af
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2022-11-25 19:22:09 +0700

    update pullback vc

--- Examples/Examples/4-Pullback/PullbackVC+Reducer.swift
+++ Examples/Examples/4-Pullback/PullbackVC+Reducer.swift
@@ -9,16 +9,6 @@ import CasePaths
 import RxComposableArchitecture
 
 internal struct Pullback: ReducerProtocol {
-    func reduce(into state: inout State, action: Action) -> RxComposableArchitecture.Effect<Action> {
-        switch action {
-        case let .textDidChange(text):
-            state.text = "You write: \(text)"
-            return .none
-        case .counter:
-            return .none
-        }
-    }
-    
     internal struct State: Equatable {
         internal var text: String = ""
         internal var counter = Counter.State(number: 0)
@@ -29,20 +19,24 @@ internal struct Pullback: ReducerProtocol {
         case counter(Counter.Action)
     }
     
-    var body: some ReducerProtocol<Pullback.State, Pullback.Action> {
-        Scope(state: \State.counter, action: /Action.counter) {
-            Counter()
-        }
-        
-        /// We can observe child action in parent reducer and do some work or additional logic here
+    var body: some ReducerProtocolOf<Pullback> {
         Reduce { state, action in
             switch action {
             case .counter(.didTapPlus):
                 print(">>> trackerDidTapPlus")
                 return .none
+
+            case let .textDidChange(text):
+                state.text = "You write: \(text)"
+                return .none
+
             default:
                 return .none
             }
+        }
+        
+        Scope(state: \.counter, action: /Action.counter) {
+            Counter()
         }
     }
 }
