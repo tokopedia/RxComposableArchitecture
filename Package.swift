ca2b7196af572633cc9288640bfa4df3354c3f49
Daniel Aditya Istyana <daniel.istyana@tokopedia.com>
2023-05-03 15:45:19 +0700

    update XCTestDynamicOverlay

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.1.1"),
         .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.8.1"),
         .package(name: "Benchmark", url: "https://github.com/google/swift-benchmark", from: "0.1.0"),
-        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.0"),
+        .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.5"),
         .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.3.0"),
         .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "0.2.0")
     ],
