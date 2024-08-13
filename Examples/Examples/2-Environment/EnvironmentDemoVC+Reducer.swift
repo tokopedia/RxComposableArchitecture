7a434c8af72b3d83da342e0d299ec57e160e8dd4
jefferson.setiawan <jefferson.setiawan@bytedance.com>
2024-08-13 21:23:07 +0700

    wip

--- Examples/Examples/2-Environment/EnvironmentDemoVC+Reducer.swift
+++ Examples/Examples/2-Environment/EnvironmentDemoVC+Reducer.swift
@@ -6,7 +6,6 @@
 //
 
 import CasePaths
-import Combine
 import Foundation
 import RxComposableArchitecture
 import RxSwift
@@ -41,9 +40,6 @@ struct Environment: ReducerProtocol {
         switch action {
         case .didLoad:
             state.isLoading = true
-            return .run { send in
-                await send(.receiveData(.success(5)))
-            }
             return env.loadData()
                 .map(Action.receiveData)
         case let .receiveData(response):
@@ -110,14 +106,9 @@ extension EnvironmentVCEnvironment: DependencyKey {
     static var liveValue: EnvironmentVCEnvironment {
         EnvironmentVCEnvironment(
             loadData: {
-                Effect.run { send in
-                    await send(.success(5))
-                }
-//                Just(.success(5))
-//                    .eraseToEffect()
-//                Observable.just(Result.success(Int.random(in: 0 ... 10000)))
-//                    .delay(.milliseconds(500), scheduler: MainScheduler.instance)
-//                    .eraseToEffect()
+                Observable.just(Result.success(Int.random(in: 0 ... 10000)))
+                    .delay(.milliseconds(500), scheduler: MainScheduler.instance)
+                    .eraseToEffect()
             },
             trackEvent: AnalyticsManager.track(_:),
             date: Date.init,
@@ -127,7 +118,7 @@ extension EnvironmentVCEnvironment: DependencyKey {
     
     static var testValue: EnvironmentVCEnvironment {
         EnvironmentVCEnvironment(
-            loadData: unimplemented("\(Self.self).loadData", placeholder: .none),
+            loadData: unimplemented("\(Self.self).loadData", placeholder: .just(.failure(CustomError(message: "Error"))).eraseToEffect()),
             trackEvent: unimplemented("\(Self.self).trackEvent"),
             date: unimplemented("\(Self.self).date", placeholder: Date()),
             uuid: unimplemented("\(Self.self).uuid", placeholder: UUID())
