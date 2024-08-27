f7c7ce1b9fe6bfaafdf9b0e0b8ccf0b7e5efbdc3
jefferson.setiawan <jefferson.setiawan@bytedance.com>
2024-08-27 16:45:19 +0700

    fix IdentifiedArray extension

--- Sources/RxComposableArchitecture/Custom/IdentifiedArray+RemoveDuplicate.swift
+++ Sources/RxComposableArchitecture/Custom/IdentifiedArray+RemoveDuplicate.swift
@@ -11,21 +11,21 @@ extension IdentifiedArrayOf where Element: Identifiable, Element.ID == ID {
     public func removeDuplicates() -> Self {
         /// This table will contain `diffIdentifier` as the `key` and object `type` as the value
         var tableOfObjectType = [AnyHashable: Any.Type]()
-
+        
         var uniqueObjects = IdentifiedArrayOf<Element>()
-
+        
         forEach { currentObject in
             /// Get current object identifier
             let currentId = currentObject.id
-
+            
             /// Get current object type from Type Erasure base object
             let currentObjectType = type(of: currentObject)
-
+            
             /// Check if `currentId` is already registered on `Table Bank of Identifiers Type`
             /// If `yes` > Get object type with current identifier from `Table Bank of Identifiers Type`
             /// If `no` > Then return `nil`
             let previousObjectType = tableOfObjectType[currentId]
-
+            
             /// Check whether current object type is the same with previous object type(if exist) fetched from `Table Bank of Identifiers Type`
             /// If `currentId` already exist on `Table Bank of Identifiers Type` but the type is different it's not counted as _**duplicates**_
             if currentObjectType != previousObjectType {
@@ -33,10 +33,12 @@ extension IdentifiedArrayOf where Element: Identifiable, Element.ID == ID {
                 uniqueObjects.append(currentObject)
             }
         }
-
+        
         return uniqueObjects
     }
-    
+}
+
+extension IdentifiedArrayOf {
     public var isNotEmpty: Bool {
         return !isEmpty
     }
