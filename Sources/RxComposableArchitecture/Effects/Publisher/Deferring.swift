3a180f7be05a7b450762896686fd7a8a58b0dd80
jefferson.setiawan <jefferson.setiawan@bytedance.com>
2024-08-27 13:33:26 +0700

    create Rx Deffered

--- Sources/RxComposableArchitecture/Effects/Publisher/Deferring.swift
+++ Sources/RxComposableArchitecture/Effects/Publisher/Deferring.swift
@@ -51,3 +51,42 @@ extension EffectPublisher {
     }
   }
 }
+
+import RxSwift
+
+extension Effect {
+    /// Returns an effect that will be executed after given `dueTime`.
+    ///
+    /// ```swift
+    /// case let .textChanged(text):
+    ///   return environment.search(text)
+    ///     .deferred(for: 0.5, scheduler: environment.mainQueue)
+    ///     .map(Action.searchResponse)
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - upstream: the effect you want to defer.
+    ///   - dueTime: The duration you want to defer for.
+    ///   - scheduler: The scheduler you want to deliver the defer output to.
+    ///   - options: Scheduler options that customize the effect's delivery of elements.
+    /// - Returns: An effect that will be executed after `dueTime`
+    public func deferred(
+        for dueTime: RxTimeInterval,
+        scheduler: SchedulerType
+    ) -> Effect<Element> {
+        switch self.operation {
+        case .none:
+            return .none
+        case .publisher, .run:
+            let publisher = Just(())
+                .asObservable()
+                .delay(dueTime, scheduler: scheduler)
+                .flatMap { self.asObservable().observeOn(scheduler)}
+                .asPublisher()
+                .catch { _ in Empty<Element, Never>(completeImmediately: true) } // Ignore errors
+                .eraseToAnyPublisher()
+            return Effect(operation: .publisher(publisher))
+        }
+    }
+}
+
